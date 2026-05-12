45494f5f72d88c9cd8ccc82f8521e44b8c0285f1
Katlego Motsepe <ntsakomotsepe7@gmail.com>
2026-05-12 23:03:25 +0200

    Fixed slide show, stuck on custom dropdown

--- north-side-web/src/app/app.config.ts
+++ north-side-web/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(MatSelectModule, MatFormFieldModule, FormsModule),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
--- north-side-web/src/app/app.ts
+++ north-side-web/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App  {
+export class App {
   protected readonly title = signal('north-side-web');
+
+  images = [
+    '/assets/spotify.png',
+    '/assets/apple.png',
+    '/assets/google.png',
+    '/assets/netflix.png',
+  ];
+  imageAlt = [
+    'Spotify',
+    'Apple',
+    'Google',
+    'Netflix',
+  ];
+
+  currentIndex = 0;
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  services = ['Web Development', 'Mobile Development', 'UI/UX Design', 'IT Support'];
+  selectedService = '';
 }
